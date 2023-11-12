c65c072265bddaf053b88f0bb558177db5fc7895
Tempest <krukovtempest@gmail.com>
2023-11-12 15:17:30 -0600

    Base Functionality
    +Added more enemies
    +Added Base health
    +Added UI for Base Health
    +Player can now 'die'

--- base.gd
+++ base.gd
@@ -1,4 +1,19 @@
 extends Node3D
 
+@export var max_health: int = 5
+@onready var label_3d = $Label3D
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		label_3d.text = str(current_health)
+		if (current_health < 1):
+			get_tree().reload_current_scene()
+	get:
+		return current_health
+func _ready():
+	# will automatically call setter
+	current_health = max_health
+
 func take_damage() -> void:
-	print("damage dealt to base")
+	# will automatically call setter
+	current_health -= 1
