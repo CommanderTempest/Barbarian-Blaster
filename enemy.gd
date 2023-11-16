93ff2fe06be59ea9ac80dc1c47dd21c9ed1736b3
CommanderTempest <krukovtempest@gmail.com>
2023-11-15 19:24:48 -0600

    Added UI & Currency
    +Added Currency UI
    +Can now earn/spend currency

--- enemy.gd
+++ enemy.gd
@@ -2,12 +2,16 @@ extends PathFollow3D
 
 @export var speed: float = 5
 @export var max_health:= 50
+@export var gold_reward:= 15
+@onready var bank = get_tree().get_first_node_in_group("bank")
+
 var current_hp: int:
 	set(health_in):
 		if (health_in < current_hp):
 			animation_player.play("TakeDamage")
 		current_hp = health_in
 		if (current_hp < 1):
+			bank.gold += gold_reward
 			queue_free()
 	
 @onready var target = get_tree().get_first_node_in_group("base")
